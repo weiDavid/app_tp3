be8a3eb8d4f39d7bbba285d4ff1ccdd33f4f0dc2
weitaotao <552063498@qq.com>
2018-11-12 17:28:47 +0800

    app-tp3

---
+++ Application/Hello/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Hello\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        echo "<h2>Hello Module</h2>";die;
+    }
+}
