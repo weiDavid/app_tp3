c3c38c55f61dea4d25e5ae3a1b76c458efecd6f6
卫涛涛 <552063498@qq.com>
2018-12-27 16:39:38 +0800

    test

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'TMPL_PARSE_STRING' => array(
         '__PUBLIC__' => __ROOT__ . '/Public',
         '__JS__' => __ROOT__ . '/Public/Js',
-        '__CSS__' => __ROOT__ . '/Public/Css',
+        '__CSS__' => __ROOT__ . '/Public/Mycss',
         '__IMAGE__' => __ROOT__ . '/Public/Image',
         '__DATA__' => __ROOT__ . '/Data/'
     ),
